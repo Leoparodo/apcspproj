6b324b985c31e0650cf7efff4379eae85d6de3a9
Leoparodo <85522623+Leoparodo@users.noreply.github.com>
2026-03-30 18:25:04 +0000

    Commented code

--- Hall-API Project.py
+++ Hall-API Project.py
@@ -1,13 +1,14 @@
 import requests
 import csv
 
+#assigns an empty variable that is used to translate the shortened language name back to the fullsized version
 lang_map = {}
-
+#allows the languages.csv to be read and lets the shortened language be expanded to full length
 with open("languages.csv", newline="", encoding="utf-8") as f:
     reader = csv.DictReader(f)
-    for row in reader:
-        srtnm = row["alpha3-b"].strip()
-        lngnm = row["English"].split(";")[0]
+    for i in reader:
+        srtnm = i["alpha3-b"].strip()
+        lngnm = i["English"].split(";")[0]
         lang_map[srtnm] = lngnm
 
 #Session data to give to OpenLibrary to get the data.
@@ -30,17 +31,17 @@ dataauth = respauth.json()
 #Sets top to None so it can be called and edited in the check.
 top = None
 #Sets top to be whatever the first mention of "top_work" is in the API for the book.
-for d in dataauth["docs"]:
-    if d.get("top_work") != None:
-        top = d.get("top_work")
+for i in dataauth["docs"]:
+    if i.get("top_work") != None:
+        top = i.get("top_work")
         break
 
 #Sets name to None so it can be called and edited in the check.
 name = None
 #Sets name to be whatever the first mention of "name" is in the API for the book.
-for d in dataauth["docs"]:
-    if d.get("name") != None:
-        name = d.get("name")
+for i in dataauth["docs"]:
+    if i.get("name") != None:
+        name = i.get("name")
         break
     
 #Gets the location data for the book results.
@@ -51,25 +52,25 @@ databook = respbook.json()
 #Sets lang to None so it can be called and edited in the check.
 lang = None
 #Sets lang to be whatever the first mention of "language" is in the API for the book.
-for d in databook.get("docs"):
-    if d.get("language") != None:
-        lang = d["language"]
+for i in databook.get("docs"):
+    if i.get("language") != None:
+        lang = i["language"]
         break  
 
-def expand_language(srtnm):
+#function that converts from shortened form of language back to full length 
+def exp_lang(srtnm):
     return lang_map.get(srtnm, srtnm)
-
 
 #Sets reyear to None so it can be called and edited in the check.
 reyear = None
 #Sets reyear to be whatever the first mention of "first_publish_year" is in the API for the book.
-for d in databook.get("docs"): 
-    if d.get("first_publish_year") != None:
-        reyear = d["first_publish_year"]
+for i in databook.get("docs"): 
+    if i.get("first_publish_year") != None:
+        reyear = i["first_publish_year"]
         break 
 
-#Joins the different languages into a list
-lang = ", ".join(expand_language(c) for c in lang)
+#Joins the different languages into a list and makes them use the standard English name instead of a shortening
+lang = ", ".join(exp_lang(i) for i in lang)
 
 #Prints what your author's top book is and what year it released.
 print(name + "'s top selling book is " + top + ", which released in " + str(reyear) + "!")
